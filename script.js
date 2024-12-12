847cd00a5f82a0d1656ad69f2492cf376ac501d5
petrichorist <2dsaulgoodmanbecomes3d@gmail.com>
2024-12-12 22:09:28 +0500

    feat: Display the players' respective choices above their score and fix some poor UI design choices

--- script.js
+++ script.js
@@ -1,96 +1,138 @@
 'use strict';
 
-document.addEventListener('DOMContentLoaded', () => {
-  let playerScore = 0;
-  let computerScore = 0;
-  const choices = ['rock', 'paper', 'scissors'];
-  let computerSelection = choices[Math.floor(Math.random() * 3)];
+let playerScore = 0;
+let computerScore = 0;
+let count = 0;
 
-  const result = document.querySelector('.result');
-  const clickBtn = document.querySelector('.clickBtn');
-  const rock = document.querySelector('#rock');
-  const paper = document.querySelector('#paper');
-  const scissors = document.querySelector('#scissors');
-  const again = document.querySelector('#again');
+const choices = ['rock', 'paper', 'scissors'];
+let computerSelection = choices[Math.floor(Math.random() * 3)];
 
-  const hScore = document.querySelector('#human-score');
-  const cScore = document.querySelector('#computer-score');
+const body = document.querySelector('body');
+const result = document.querySelector('.result');
+const clickBtn = document.querySelector('.clickBtn');
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const again = document.querySelector('.again');
 
-  function computerChoice() {
-    computerSelection = choices[Math.floor(Math.random() * 3)];
-  }
+const hScore = document.querySelector('#human-score');
+const cScore = document.querySelector('#computer-score');
+const hObj = document.querySelector('#human-object');
+const cObj = document.querySelector('#computer-object');
 
-  function disableBtns(bool) {
-    rock.disabled = bool;
-    paper.disabled = bool;
-    scissors.disabled = bool;
-  }
+function computerChoice() {
+  computerSelection = choices[Math.floor(Math.random() * 3)];
+}
 
-  function reveal() {
-    clickBtn.classList.add('hidden');
-    result.classList.remove('hidden');
-    result.classList.add('reveal');
-  }
+function disableBtns(bool) {
+  rock.disabled = bool;
+  paper.disabled = bool;
+  scissors.disabled = bool;
+}
 
-  function playGame(pick) {
-    function playRound(playerSelection, computerSelection) {
-      if (
-        (playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'paper' && computerSelection == 'rock') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper')
-      ) {
-        result.textContent = `You WIN! ${playerSelection} beats ${computerSelection}.`;
-        playerScore++;
-        hScore.textContent = playerScore;
-        if (playerScore == 5) {
-          result.textContent = 'Player wins!';
-          disableBtns(true);
-        }
-      } else if (playerSelection == computerSelection) {
-        result.textContent = `It's a DRAW! You both chose to play... ${playerSelection}`;
-      } else {
-        result.textContent = `You LOSE! ${computerSelection} beats ${playerSelection}.`;
-        computerScore++;
-        cScore.textContent = computerScore;
-        if (computerScore == 5) {
-          result.textContent = `Computer wins! Try again.`;
-          disableBtns(true);
-        }
+function reveal() {
+  clickBtn.classList.add('hidden');
+  result.classList.remove('hidden');
+  result.classList.add('reveal');
+}
+
+function playGame(pick) {
+  hObj.textContent = '';
+  cObj.textContent = '';
+
+  function playRound(playerSelection, computerSelection) {
+    if (
+      (playerSelection == 'rock' && computerSelection == 'scissors') ||
+      (playerSelection == 'paper' && computerSelection == 'rock') ||
+      (playerSelection == 'scissors' && computerSelection == 'paper')
+    ) {
+      result.textContent = `You WIN! ${playerSelection} beats ${computerSelection}.`;
+      playerScore++;
+      hScore.textContent = playerScore;
+
+      if (playerSelection === 'rock') {
+        hObj.textContent = '🪨';
+      } else if (playerSelection === 'paper') {
+        hObj.textContent = '💶';
+      } else if (playerSelection === 'scissors') {
+        hObj.textContent = '✂';
+      }
+
+      if (computerSelection === 'rock') {
+        cObj.textContent = '🪨';
+      } else if (computerSelection === 'paper') {
+        cObj.textContent = '💶';
+      } else if (computerSelection === 'scissors') {
+        cObj.textContent = '✂';
+      }
+
+      if (playerScore == 5) {
+        result.textContent = 'Player wins!';
+        disableBtns(true);
+      }
+    } else if (playerSelection == computerSelection) {
+      result.textContent = `It's a DRAW! You both chose to play... ${playerSelection}`;
+    } else {
+      result.textContent = `You LOSE! ${computerSelection} beats ${playerSelection}.`;
+      computerScore++;
+      cScore.textContent = computerScore;
+
+      if (playerSelection === 'rock') {
+        hObj.textContent = '🪨';
+      } else if (playerSelection === 'paper') {
+        hObj.textContent = '💶';
+      } else if (playerSelection === 'scissors') {
+        hObj.textContent = '✂';
+      }
+
+      if (computerSelection === 'rock') {
+        cObj.textContent = '🪨';
+      } else if (computerSelection === 'paper') {
+        cObj.textContent = '💶';
+      } else if (computerSelection === 'scissors') {
+        cObj.textContent = '✂';
+      }
+
+      if (computerScore == 5) {
+        result.textContent = `Computer wins! Try again.`;
+        disableBtns(true);
       }
     }
-
-    playRound(pick, computerSelection);
   }
 
-  rock.addEventListener('click', () => {
-    playGame('rock');
-    computerChoice();
-    reveal();
-  });
+  playRound(pick, computerSelection);
+}
 
-  paper.addEventListener('click', () => {
-    playGame('paper');
-    computerChoice();
-    reveal();
-  });
+rock.addEventListener('click', () => {
+  playGame('rock');
+  computerChoice();
+  reveal();
+});
 
-  scissors.addEventListener('click', () => {
-    playGame('scissors');
-    computerChoice();
-    reveal();
-  });
+paper.addEventListener('click', () => {
+  playGame('paper');
+  computerChoice();
+  reveal();
+});
 
-  again.addEventListener('click', () => {
-    playerScore = 0;
-    computerScore = 0;
+scissors.addEventListener('click', () => {
+  playGame('scissors');
+  computerChoice();
+  reveal();
+});
 
-    disableBtns(false);
+again.addEventListener('click', () => {
+  playerScore = 0;
+  computerScore = 0;
 
-    hScore.textContent = playerScore;
-    cScore.textContent = computerScore;
+  disableBtns(false);
 
-    clickBtn.classList.remove('hidden');
-    result.classList.add('hidden');
-    result.textContent = '';
-  });
+  hScore.textContent = playerScore;
+  cScore.textContent = computerScore;
+  hObj.textContent = '';
+  cObj.textContent = '';
+
+  clickBtn.classList.remove('hidden');
+  result.classList.add('hidden');
+  result.textContent = '';
 });
